01344cfd0092ce3c88921c3a34d1d17311242398
LHNB521 <1767359165@qq.com>
2024-01-19 10:19:38 +0800

    权限接口完美

--- src/modules/permission/permission.controller.ts
+++ src/modules/permission/permission.controller.ts
@@ -1,7 +1,17 @@
-import { JwtGuard } from '@/common/guards';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { JwtGuard, PreviewGuard } from '@/common/guards';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { PermissionService } from './permission.service';
-import { CreatePermissionDto } from './dto';
+import { CreatePermissionDto, UpdatePermissionDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('permission')
@@ -28,5 +38,33 @@ export class PermissionController {
   @Get('tree')
   findAllTree() {
     return this.permissionService.findAllTree();
+  }
+
+  @Get('menu/tree')
+  findMenuTree() {
+    return this.permissionService.findMenuTree();
+  }
+
+  @Patch('update/:id')
+  @UseGuards(PreviewGuard)
+  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
+    return this.permissionService.update(+id, updatePermissionDto);
+  }
+
+  @Delete('delete/:id')
+  @UseGuards(PreviewGuard)
+  delete(@Param('id') id: string) {
+    return this.permissionService.remove(+id);
+  }
+
+  @Get('button-and-api/:parentId')
+  findButtonAndApi(@Param('parentId') parentId: string) {
+    return this.permissionService.findButtonAndApi(+parentId);
+  }
+
+  /* 校验 path 存不存在menu资源内  */
+  @Get('menu/validate')
+  validateMenuPath(@Query('path') path: string) {
+    return this.permissionService.validateMenuPath(path);
   }
 }
--- src/modules/permission/permission.service.ts
+++ src/modules/permission/permission.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Permission } from './permission.entity';
-import { Repository } from 'typeorm';
-import { CreatePermissionDto } from './dto';
+import { In, Repository } from 'typeorm';
+import { CreatePermissionDto, UpdatePermissionDto } from './dto';
 import { SharedService } from '@/shared/shared.service';
 
 @Injectable()
@@ -26,12 +26,60 @@ export class PermissionService {
   findAll() {
     return this.permissionRepo.find({ where: { type: 'MENU' } });
   }
-  // 获取树形菜单
+  // 获取树形权限
   async findAllTree() {
     const permissions = await this.permissionRepo.find({
       order: { order: 'ASC' },
       where: { type: 'MENU' },
     });
     return this.sharedService.handleTree(permissions);
+  }
+
+  // 获取菜单树
+  async findMenuTree() {
+    const permissions = await this.permissionRepo.find({
+      order: { order: 'ASC' },
+      where: { type: 'MENU' },
+    });
+    return this.sharedService.handleTree(permissions);
+  }
+
+  // 更新权限
+  async update(id: number, updatePermissionDto: UpdatePermissionDto) {
+    const permission = await this.permissionRepo.findOne({ where: { id } });
+    if (!permission) throw new BadRequestException('权限不存在或者已删除');
+    const newPermission = this.permissionRepo.merge(permission, updatePermissionDto);
+    await this.permissionRepo.save(newPermission);
+    return true;
+  }
+
+  // 递归删除所有子孙权限
+  async remove(id: number) {
+    const permission = await this.permissionRepo.findOne({
+      where: { id },
+      relations: { roles: true },
+    });
+    if (!permission) throw new BadRequestException('权限不存在或者已删除');
+    if (permission.roles?.length) {
+      throw new BadRequestException('当前权限存在已授权的角色，不允许删除！');
+    }
+    await this.permissionRepo.remove(permission);
+    return true;
+  }
+
+  // 获取按钮和接口权限
+  findButtonAndApi(parentId: number) {
+    return this.permissionRepo.find({
+      where: { parentId, type: In(['BUTTON', 'API']) },
+    });
+  }
+
+  // 验证菜单路径是否存在
+  async validateMenuPath(path: string) {
+    const findMenu = await this.permissionRepo.findOne({
+      where: { path: path, type: 'MENU' },
+    });
+    if (findMenu) return true;
+    return false;
   }
 }
